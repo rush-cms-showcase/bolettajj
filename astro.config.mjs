91ed05fbaff3480f6033e8a255d7ee14fce0022f
Rafhael Marsigli <rmarsigli@gmail.com>
2026-03-24 11:06:28 -0300

    chore(config): add project dependencies, path aliases and plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwind(),
+      Icons({ compiler: 'astro' }),
+    ],
+  },
+})
